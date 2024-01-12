1ffde340234aaf56f304d93bc4854584e003d719
Rodrigo Rodrigues <rs9844660@gmail.com>
2024-01-12 16:47:57 -0300

    Configurando variaveis

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,20 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function styles() {
-    gulp.src('./src/styles/main.scss')
+    return gulp.src('./src/styles/main.scss')
     .pipe(sass({outputStyle: 'compressed'}))
     .pipe(gulp.dest('./dist/css'))
 }
 
+function images() {
+    return gulp.src('./src/images/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/images'))
+}
 
-exports.default = gulp.parallel(styles);
-exports.watch = () => {
+exports.default = gulp.parallel(styles, images);
+exports.watch = function() {
     gulp.watch('./src/styles/*.scss', {ignoreInitial: false} , gulp.parallel(styles));
 }
