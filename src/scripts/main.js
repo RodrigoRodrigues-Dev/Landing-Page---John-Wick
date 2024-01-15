256cd0ca3b4ac318b84159d8d12a56ad5f5f159c
Rodrigo Rodrigues <rs9844660@gmail.com>
2024-01-15 15:41:45 -0300

    section log in finalizada

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,39 +1,41 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const buttonsNav = document.querySelectorAll('[data-nav-button]')
-    const buttonClose = document.querySelector('.button-close')
+    const buttonsNav = document.querySelectorAll('[data-nav-button]');
+    const buttonClose = document.querySelectorAll('.button-close');
 
-    for (let i = 0 ; i < buttonsNav.length; i++) {
-        buttonsNav[i].addEventListener('click', (botao) => {
-            const Alvo = botao.target.parentNode.dataset.navButton
-            const section = document.querySelector(`[data-section-id="${Alvo}"]`)
-            
-            esconderConteudoHover()
-            section.classList.add('section--is-active')
-        })
-    }
+    buttonsNav.forEach(button => {
+        button.addEventListener('click', (event) => {
+            const targetId = event.currentTarget.dataset.navButton;
+            const section = document.querySelector(`[data-section-id="${targetId}"]`);
 
-    buttonClose.addEventListener('click', () => {
-        const sections = document.querySelectorAll('section')
-
-        sections.forEach(section => {
-            section.classList.remove('section--is-active');
+            event.preventDefault();
+            hideContentHover();
+            section.classList.add('section--is-active');
         });
-        mostrarConteudoHover()
-    })
+    });
 
-})
+    buttonClose.forEach(button => {
+        button.addEventListener('click', () => {
+            const sections = document.querySelectorAll('section');
 
-function esconderConteudoHover() {
+            sections.forEach(section => {
+                section.classList.remove('section--is-active');
+            });
+            showContentHover();
+        });
+    });
+});
+
+function hideContentHover() {
     const containers = document.querySelectorAll('.container');
-    
-    containers.forEach(section => {
-        section.classList.remove('container--is-active');
+
+    containers.forEach(container => {
+        container.classList.remove('container--is-active');
     });
 }
 
-function mostrarConteudoHover() {
+function showContentHover() {
     const containers = document.querySelectorAll('.container');
-    
+
     containers.forEach(container => {
         container.classList.add('container--is-active');
     });
