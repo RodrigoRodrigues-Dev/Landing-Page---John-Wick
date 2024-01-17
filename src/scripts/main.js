3750007acb1d85728182b2f09a44873bad0efe7e
Rodrigo Rodrigues <rs9844660@gmail.com>
2024-01-17 15:28:06 -0300

    finalizando menu mobile e deixando o site responsivo

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,26 @@ document.addEventListener('DOMContentLoaded', () => {
     const buttonClose = document.querySelectorAll('.button-close');
     const buttonTickets = document.querySelector('[data-story-button]');
     const video = document.querySelector('video');
+    const buttonShowMenuMobile = document.querySelector('[data-mobile-button="open"]');
+    const buttonHideMenuMobile = document.querySelector('[data-mobile-button="close"]');
+
+    //exibe menu mobile e remove o search mobile
+    buttonShowMenuMobile.addEventListener('click',() => {
+        const menu = document.querySelector(`[data-mobile-menu="menu"]`)
+        const menuSearch = document.querySelector('.mobile__search')
+
+        menu.classList.add('menu-mobile--is-active')
+        menuSearch.classList.remove('mobile__search--is-active')
+    })
+
+     //remove menu mobile e adiciona o search mobile
+    buttonHideMenuMobile.addEventListener('click',() => {
+        const menu = document.querySelector(`[data-mobile-menu="menu"]`)
+        const menuSearch = document.querySelector('.mobile__search')
+
+        menu.classList.remove('menu-mobile--is-active')
+        menuSearch.classList.add('mobile__search--is-active')
+    })
 
     buttonsNav.forEach(button => {
         button.addEventListener('click', (event) => {
@@ -16,6 +36,7 @@ document.addEventListener('DOMContentLoaded', () => {
             section.classList.add('section--is-active');
             header.classList.remove('header--is-active');
 
+            // Inicia a reprodução do vídeo se o botão clicado for o de trailer
             if (targetId === 'trailer') {
                 video.play();
             }
@@ -26,6 +47,7 @@ document.addEventListener('DOMContentLoaded', () => {
         button.addEventListener('click', () => {
             hideSection();
             showContentHover();
+            // Pausa a reprodução do vídeo
             video.pause();
         });
     });
@@ -33,16 +55,18 @@ document.addEventListener('DOMContentLoaded', () => {
     buttonTickets.addEventListener('click', () => {
         const soldOut = document.querySelector('.story__content__soldOut');
     
+        // Se "Sold Out" existir, adiciona classe para exibir e remove após 1 segundo
         if (soldOut) {
             soldOut.classList.add('story__content__soldOut--is-active');
-        }
 
-        setTimeout(() => {
-            soldOut.classList.remove('story__content__soldOut--is-active');
-        }, 1000);
+            setTimeout(() => {
+                soldOut.classList.remove('story__content__soldOut--is-active');
+            }, 1000);
+        }
     });
 });
 
+// Função para esconder o conteúdo do hover
 function hideContentHover() {
     const containers = document.querySelectorAll('.container');
 
@@ -53,6 +77,7 @@ function hideContentHover() {
     });
 }
 
+// Função para exibir novamente o conteúdo do hover
 function showContentHover() {
     const containers = document.querySelectorAll('.container');
     const header = document.querySelector('.header');
@@ -60,9 +85,11 @@ function showContentHover() {
     containers.forEach(container => {
         container.classList.add('container--is-active');
     });
+    // Adiciona a classe ativa ao cabeçalho
     header.classList.add('header--is-active')
 }
 
+// Função para esconder qualquer seção ativa
 function hideSection() {
     const sections = document.querySelectorAll('section');
 
